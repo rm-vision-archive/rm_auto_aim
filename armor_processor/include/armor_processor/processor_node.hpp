476098a43f8af59050d575f435696142a0e04359
chenjun <chen.junn@outlook.com>
2023-03-26 23:42:25 +0800

    Fixed processor test failure

--- armor_processor/include/armor_processor/processor_node.hpp
+++ armor_processor/include/armor_processor/processor_node.hpp
@@ -5,13 +5,13 @@
 
 // ROS
 #include <message_filters/subscriber.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/create_timer_ros.h>
 #include <tf2_ros/message_filter.h>
 #include <tf2_ros/transform_listener.h>
 
 #include <rclcpp/rclcpp.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
 
 // STD
